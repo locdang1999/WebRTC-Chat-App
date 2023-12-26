1f3f16d6de3c6f14ced62b6845a15cd99c8c1cf8
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2023-12-26 18:02:09 +0700

    Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { Typography, AppBar } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import VideoPlayer from "./components/VideoPlayer";
+import Options from "./components/Options";
+import Notifications from "./components/Notifications";
+
+const theme = createTheme();
+
+const useStyles = makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px !important",
+    display: "flex !important",
+    flexDirection: "row !important",
+    justifyContent: "center !important",
+    alignItems: "center !important",
+    width: "600px !important",
+    border: "2px solid black !important",
+
+    [theme.breakpoints.down("sm")]: {
+      width: "90% !important",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
 
 function App() {
-  return <div className="App">Video Chat App</div>;
+  const classes = useStyles();
+  return (
+    // <ThemeProvider theme={theme}>
+      <div className={classes.wrapper}>
+        <AppBar position="static" color="inherit" className={classes.appBar}>
+          <Typography variant="h2" align="center">
+            Video Chat
+          </Typography>
+        </AppBar>
+        {/* VieoPlayer */}
+        <VideoPlayer />
+        {/* Options -> Notifications */}
+        <Options>
+          <Notifications />
+        </Options>
+      </div>
+    // </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ client/src/components/Notifications.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { Button } from "@mui/material";
+import { SocketContext } from "../socketContext";
+
+const Notifications = () => {
+  const { answerCall, call, callAccepted } = useContext(SocketContext);
+  console.log(call, callAccepted)
+
+  return (
+    <>
+      {call.isReceivedCall && !callAccepted && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <h1>{call.name} is calling: </h1>
+          <Button variant="contained" color="primary" onClick={answerCall}>
+            Answer
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Notifications;
